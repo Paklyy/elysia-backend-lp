6e75f5728b43e8e786c9e2ffc1af6a9f442d1e0a
Pedro Bim <pedrodinizz2@outlook.com>
2025-10-31 16:42:15 -0300

    Merge dev / biome adjustments

--- src/mail/client.ts
+++ src/mail/client.ts
@@ -1,4 +1,4 @@
-import { Resend } from "resend";
-import { env } from "../util/env";
+import { Resend } from 'resend'
+import { env } from '../util/env'
 
-export const resend = new Resend(env.RESEND_API_KEY);
+export const resend = new Resend(env.RESEND_API_KEY)
