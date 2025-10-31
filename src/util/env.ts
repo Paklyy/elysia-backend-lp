a5f07d75a64c9e6994ee1fd21229865ad2bf7883
Pedro Bim <pedrodinizz2@outlook.com>
2025-10-31 20:07:47 -0300

    Fix zod error

--- src/util/env.ts
+++ src/util/env.ts
@@ -4,7 +4,7 @@ const envSchema = z.object({
 	DATABASE_URL: z.url().min(1),
 	RESEND_API_KEY: z.string().min(1),
 	RESEND_EMAIL: z.email().min(1),
-	PORT: z.int(),
+	PORT: z.coerce.number().default(3333),
 })
 
 export const env = envSchema.parse(process.env)
